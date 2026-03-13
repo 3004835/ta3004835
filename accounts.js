69b56adb6ba4030cbc39a8941e2b41dcd0877a0a
3004835 <maxblox138@gmail.com>
2026-03-13 07:36:03 -0400

    Add files via upload

---
+++ accounts.js
@@ -0,0 +1,19 @@
+// Account data for Site Unblocker
+const accountData = [
+    {
+        "Username": "Andy",
+        "Password": "Jaki7767",
+        "Access": false,
+        "Start": "",
+        "End": "",
+        "type": "Permanent Account"
+    },
+    {
+        "Username": "Dominic", 
+        "Password": "Dom123456",
+        "Access": true,
+        "Start": "",
+        "End": "",
+        "type": "Permanent Account"
+    }
+];
